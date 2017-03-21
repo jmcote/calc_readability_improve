ca4f4ce663fa5e9b4eec8055b729320fc07b58ba
jmcote <jcote.ts@gmail.com>
2017-03-21 14:33:49 -0700

    first half of functions added to object

--- calculator_start.js
+++ calculator_start.js
@@ -1,17 +1,37 @@
-
-var current_input = "0";
-var memory = "0";
-var operator = 0;
-var other_memory = current_input;
-var trig_mode = "Radian";
-var divideByZero = false;
-var tooManyDigits = false;
+function Calculator(){
+    this.currentInput = "0";
+    this.memory = "0";
+    this.operator = 0;
+    this.otherMemory = this.currentInput;
+    this.trigMode = "Radian";
+    this.divideByZero = false;
+    this.tooManyDigits = false;
+    this.displayCurrentInput = displayCurrentInput;
+    this.addDigit = addDigit;
+    this.this.addDecimal = addDecimal;
+    this.allClear = allClear;
+    this.storeOperator = storeOperator;
+    this.calculate = calculate;
+    this.changeSign = changeSign;
+    this.pleaseClear = pleaseClear;
+    this.percentage = percentage;
+    this.factorial = factorial;
+    this.factorial = factorial;
+    this.squareRoot = squareRoot;
+    this.inverse = inverse;
+    this.pi = pi;
+    this.mySin = mySin;
+    this.myCos = myCos;
+    this.myTan = myTan;
+    this.checkZero = checkZero;
+    this.toRadian = toRadian;
+}
 
 /**
  * Helper function for displaying the current input
  */
-function displayCurrentInput() {
-    document.getElementById('screen').value = current_input;
+function displayCurrentInput {
+    document.getElementById('screen').value = this.currentInput;
 }
 
 /**
@@ -19,48 +39,48 @@ function displayCurrentInput() {
  * @param {number} dig The digit being added
  */
 function addDigit(dig) {
-    tooManyDigits = false;
-    if (current_input.length + 1 > 18) {
+    this.tooManyDigits = false;
+    if (this.currentInput.length + 1 > 18) {
         alert("No more than 18 characters");
-        tooManyDigits = true;
+        this.tooManyDigits = true;
     }
-    if ((eval(current_input) == 0) && (current_input.indexOf(".") == -1)) {
-        current_input = dig;
+    if ((eval(this.currentInput) == 0) && (this.currentInput.indexOf(".") == -1)) {
+        this.currentInput = dig;
     }
     else {
-        current_input = current_input + dig;
+        this.currentInput = this.currentInput + dig;
     }
-    displayCurrentInput();
+    this.displayCurrentInput;
 }
 
 /**
  * Adds a decimal to the current input
  */
 function addDecimal() {
-    if (current_input.length == 0) {
+    if (this.currentInput.length == 0) {
         //no leading ".", use "0."
-        current_input = "0.";
+        this.currentInput = "0.";
     }
     else {
         // First make sure one doesn't exist
-        if (current_input.indexOf(".") == -1) {
-            current_input = current_input + ".";
+        if (this.currentInput.indexOf(".") == -1) {
+            this.currentInput = this.currentInput + ".";
         }
     }
-    displayCurrentInput();
+    this.displayCurrentInput;
 }
 
 /**
  * Clears everything.
  */
 function allClear() {
-    current_input = "";
+    this.currentInput = "";
     console.log("Current input cleared");
-    operator = 0; //clear operator
+    this.operator = 0; //clear operator
     console.log("Operator cleared");
-    memory = "0"; //clear memory
+    this.memory = "0"; //clear this.memory
     console.log("Memory cleared");
-    displayCurrentInput();
+    this.displayCurrentInput;
 }
 /**
  * Stores the last operator pushed for multiply, divide, add, subtract, or an exponential operation.
@@ -68,107 +88,107 @@ function allClear() {
  */
 function storeOperator(op) {
     if (op.indexOf("*") > -1) {
-        operator = 1;
+        this.operator = 1;
     }; //codes for *
     if (op.indexOf("/") > -1) {
-        operator = 2;
+        this.operator = 2;
     }; // slash (divide)
     if (op.indexOf("+") > -1) {
-        operator = 3;
+        this.operator = 3;
     }; // sum
     if (op.indexOf("-") > -1) {
-        operator = 4;
+        this.operator = 4;
     }; // difference
     if (op.indexOf("^") > -1) {
-        operator = 5;
+        this.operator = 5;
         // exponent
     };
-    memory = current_input;  //store value
-    current_input = "";      //Set current input to nothing to show that the user has done something
-    displayCurrentInput();
+    this.memory = this.currentInput;  //store value
+    this.currentInput = "";      //Set current input to nothing to show that the user has done something
+    this.displayCurrentInput;
     console.log(op);
-    console.log(operator);
-    console.log(memory);
+    console.log(this.operator);
+    console.log(this.memory);
 }
 
 /**
  * Calculate using operator, the memory and what is current
  */
 function calculate() {
-    divideByZero = false;
-    if (operator == 1) {
-        current_input = eval(memory) * eval(current_input);
+    this.divideByZero = false;
+    if (this.operator == 1) {
+        this.currentInput = eval(this.memory) * eval(this.currentInput);
     };
-    if (operator == 2) {
-        if (eval(current_input) == 0) {
-            current_input = "undefined- cannot divide by zero";
-            divideByZero = true;
+    if (this.operator == 2) {
+        if (eval(this.currentInput) == 0) {
+            this.currentInput = "undefined- cannot divide by zero";
+            this.divideByZero = true;
         }
         else {
-            current_input = eval(memory) / eval(current_input);
+            this.currentInput = eval(this.memory) / eval(this.currentInput);
         }
     };
-    if (operator == 3) {
-        current_input = eval(memory) + eval(current_input);
+    if (this.operator == 3) {
+        this.currentInput = eval(this.memory) + eval(this.currentInput);
     };
-    if (operator == 4) {
-        current_input = eval(memory) - eval(current_input);
+    if (this.operator == 4) {
+        this.currentInput = eval(this.memory) - eval(this.currentInput);
     };
-    if (operator == 5 && current_input < 0) {
-        var power_of = -1 * eval(current_input);
-        var base = eval(memory);
+    if (this.operator == 5 && this.currentInput < 0) {
+        this.power_of = -1 * eval(this.currentInput);
+        var base = eval(this.memory);
         var final = 1;
         for (i = 0; i < power_of; i++) {
             final = final * base;
             console.log(base + "^" + (i + 1) + " = " + final);
         }
-        current_input = 1 / final;
+        this.currentInput = 1 / final;
     }
-    else if (operator == 5) {
-        var power_of = eval(current_input);
-        var base = eval(memory);
+    else if (this.operator == 5) {
+        var power_of = eval(this.currentInput);
+        var base = eval(this.memory);
         var final = 1;
         for (i = 0; i < power_of; i++) {
             final = final * base;
             console.log(base + "^" + (i + 1) + " = " + final);
         }
-        current_input = final;
+        this.currentInput = final;
     };
-    operator = 0; //clear operator
-    memory = "0"; //clear memory
-    displayCurrentInput();
+    this.operator = 0; //clear operator
+    this.memory = "0"; //clear memory
+    this.displayCurrentInput;
 }
 
 /**
  * Change the sign of the current input
  */
 function changeSign() {
-    if (current_input == 0 || current_input == ""){
+    if (this.currentInput == 0 || this.currentInput == ""){
         return;
     }
-    current_input = -1 * current_input;
-    displayCurrentInput();
+    this.currentInput = -1 * this.currentInput;
+    this.displayCurrentInput;
 }
 
 /**
  * Clear the current input back to 0
  */
 function pleaseClear() {
-    current_input = "";
+    this.currentInput = "";
     console.log("Current input cleared");
-    console.log("Operator remains " + operator + " and memory remains " + memory);
-    displayCurrentInput();
+    console.log("Operator remains " + this.operator + " and memory remains " + this.memory);
+    this.displayCurrentInput;
 }
 
 /**
  * Change the current input to a percentage
  */
 function percentage() {
-    if (current_input.indexOf("%") == -1) {
-        current_input = current_input * 100;
-        current_input = current_input.toString() + "%";
+    if (this.currentInput.indexOf("%") == -1) {
+        this.currentInput = this.currentInput * 100;
+        this.currentInput = this.currentInput.toString() + "%";
     }
-    displayCurrentInput();
+    this.displayCurrentInput;
 }
 
 /**
@@ -177,51 +197,51 @@ function percentage() {
 function factorial() {
     var i = "";
     var fact = 1;
-    if (current_input == 0) {       // factorial of 0 is 1
-        current_input = 1;
-    } else if (current_input < 0) {        //negative numbers cant be factorials
-        current_input = "ERROR- input cannot be less than 0";
+    if (this.currentInput == 0) {       // factorial of 0 is 1
+        this.currentInput = 1;
+    } else if (this.currentInput < 0) {        //negative numbers cant be factorials
+        this.currentInput = "ERROR- input cannot be less than 0";
     }
     else {
-        for (i = 1; i <= current_input; i++) {
+        for (i = 1; i <= this.currentInput; i++) {
             fact = fact * i;
         }
-        current_input = fact;
+        this.currentInput = fact;
     }
-    displayCurrentInput();
+    this.displayCurrentInput;
 }
 
 /**
  * Calculate the square of the current input
  */
 function square() {
-    current_input = current_input * current_input;
-    displayCurrentInput();
+    this.currentInput = this.currentInput * this.currentInput;
+    this.displayCurrentInput;
 }
 
 /**
  * Calculate the square root of the current input, including i for negative inputs
  */
 function squareRoot() {
-    if (current_input == -1) {
-        current_input = "i";
+    if (this.currentInput == -1) {
+        this.currentInput = "i";
     }
-    else if (current_input < 0) {
-        current_input = Math.sqrt(current_input * -1);
-        current_input = current_input.toString() + "i";
+    else if (this.currentInput < 0) {
+        this.currentInput = Math.sqrt(this.currentInput * -1);
+        this.currentInput = this.currentInput.toString() + "i";
     }
     else {
-        current_input = Math.sqrt(current_input);
+        this.currentInput = Math.sqrt(this.currentInput);
     }
-    displayCurrentInput();
+    this.displayCurrentInput;
 }
 
 /**
  * Calculate the inverse of the current input
  */
 function inverse() {
-    current_input = 1 / current_input;
-    displayCurrentInput();
+    this.currentInput = 1 / this.currentInput;
+    this.displayCurrentInput;
 }
 
 
@@ -231,78 +251,78 @@ function inverse() {
  * Sets current input to pi
  */
 function pi() {
-    current_input = Math.PI;
-    displayCurrentInput();
+    this.currentInput = Math.PI;
+    this.displayCurrentInput;
 }
 
 /**
  * Sin of current input
  */
 function mySin() {
-    if (current_input === ""){
+    if (this.currentInput === ""){
         return;
     }
-    if (trig_mode == "Degree") {
-        var tempRad = calcToRadian(current_input);
-        current_input = Math.sin(tempRad);
-    } else if (trig_mode == "Radian") {
-        current_input = Math.sin(current_input);
+    if (this.trigMode == "Degree") {
+        var tempRad = calcToRadian(this.currentInput);
+        this.currentInput = Math.sin(tempRad);
+    } else if (this.trigMode == "Radian") {
+        this.currentInput = Math.sin(this.currentInput);
     } else {
-        current_input = "Invalid trig mode";
+        this.currentInput = "Invalid trig mode";
     }
-    //current_input = Math.sin(current_input);
-    displayCurrentInput();
-    checkZero();
+    //this.currentInput = Math.sin(this.currentInput);
+    this.displayCurrentInput;
+    this.checkZero();
 }
 
 /**
  * Finds cosine of current inout
  */
 function myCos() {
-    if (current_input === ""){
+    if (this.currentInput === ""){
         return;
     }
-    if (trig_mode == "Degree") {
-        var tempRad = calcToRadian(current_input);
-        current_input = Math.cos(tempRad);
-    } else if (trig_mode == "Radian") {
-        current_input = Math.cos(current_input);
+    if (this.trigMode == "Degree") {
+        var tempRad = calcToRadian(this.currentInput);
+        this.currentInput = Math.cos(tempRad);
+    } else if (this.trigMode == "Radian") {
+        this.currentInput = Math.cos(this.currentInput);
     } else {
-        current_input = "Invalid trig mode";
+        this.currentInput = "Invalid trig mode";
     }
-    //current_input = Math.cos(current_input);
-    displayCurrentInput();
-    checkZero();
+    //this.currentInput = Math.cos(this.currentInput);
+    this.displayCurrentInput;
+    this.checkZero();
 }
 
 /**
  * Finds tanget of current input
  */
 function myTan() {
-    if (current_input == 0 || current_input == ""){
+    if (this.currentInput == 0 || this.currentInput == ""){
         return;
     }
-    if (trig_mode == "Degree") {
-        var tempRad = calcToRadian(current_input);
-        current_input = Math.tan(tempRad);
-    } else if (trig_mode == "Radian") {
-        current_input = Math.tan(current_input);
+    if (this.trigMode == "Degree") {
+        var tempRad = calcToRadian(this.currentInput);
+        this.currentInput = Math.tan(tempRad);
+    } else if (this.trigMode == "Radian") {
+        this.currentInput = Math.tan(this.currentInput);
     } else {
-        current_input = "Invalid trig mode";
+        this.currentInput = "Invalid trig mode";
     }
-    current_input = Math.tan(current_input);
-    displayCurrentInput();
-    console.log(current_input.toString().indexOf("e-"));
-    checkZero();
+    this.currentInput = Math.tan(this.currentInput);
+    this.displayCurrentInput;
+    console.log(this.currentInput.toString().indexOf("e-"));
+    this.checkZero();
 }
 
 /**
  * Sets extremley small number to zero
  */
 function checkZero() {
-    if (current_input.toString().indexOf("e-") !== -1 ) {
-        current_input = 0;
-        displayCurrentInput();
+    if (this.currentInput.toString().indexOf("e-") !== -1 ) {
+        this.currentInput = 0;
+        this.displayCurrentInput;
     }
 }
 
@@ -310,11 +330,11 @@ function checkZero() {
  * Converts current input from degrees to radian
  */
 function toRadian() {
-    if (current_input == 0 || current_input == ""){
+    if (this.currentInput == 0 || this.currentInput == ""){
         return;
     }
-    current_input = current_input * (Math.PI / 180);
-    displayCurrentInput();
+    this.currentInput = this.currentInput * (Math.PI / 180);
+    this.displayCurrentInput;
 }
 
 /**
@@ -329,11 +349,11 @@ function calcToRadian(number) {
  * Converts current input from radians to degrees
  */
 function toDegree() {
-    if (current_input == 0 || current_input == ""){
+    if (this.currentInput == 0 || this.currentInput == ""){
         return;
     }
-    current_input = current_input * (180 / Math.PI);
-    displayCurrentInput();
+    this.currentInput = this.currentInput * (180 / Math.PI);
+    this.displayCurrentInput;
 }
 
 /**
@@ -348,16 +368,16 @@ function calcToDegree(number) {
  * Converts trig mode to degrees
  */
 function modeDegree(id) {
-    trig_mode = "Degree";
-    document.getElementById(id).innerHTML = "TRIGMODE: " + trig_mode;
+    this.trigMode = "Degree";
+    document.getElementById(id).innerHTML = "TRIGMODE: " + this.trigMode;
 }
 
 /**
  * Converts trig mode to radians
  */
 function modeRadian(id) {
-    trig_mode = "Radian";
-    document.getElementById(id).innerHTML = "TRIGMODE: " + trig_mode;
+    this.trigMode = "Radian";
+    document.getElementById(id).innerHTML = "TRIGMODE: " + this.trigMode;
 }
 
 // memory functions
@@ -366,32 +386,32 @@ function modeRadian(id) {
  * Sets current input as memory
  */
 function ms() {
-    other_memory = current_input;
-    console.log(other_memory);
+    this.otherMemory = this.currentInput;
+    console.log(this.otherMemory);
 }
 
 /**
  * Tells user what the memory is
  */
 function mr() {
-    current_input = other_memory;
-    displayCurrentInput();
-    console.log("memory is " + other_memory);
+    this.currentInput = this.otherMemory;
+    this.displayCurrentInput;
+    console.log("memory is " + this.otherMemory);
 }
 
 /**
  * Adds current input to memory
  */
 function mplus() {
-    other_memory = eval(other_memory) + eval(current_input);
-    console.log("memory is " + other_memory);
+    this.otherMemory = eval(this.otherMemory) + eval(this.currentInput);
+    console.log("memory is " + this.otherMemory);
 }
 
 /**
  * Subtracts current input from the memory
  */
 function mminus() {
-    other_memory = eval(other_memory) - eval(current_input);
+    this.otherMemory = eval(this.otherMemory) - eval(this.currentInput);
 }
 
 /**
@@ -399,7 +419,7 @@ function mminus() {
  * @returns {boolean} Whether a too many digits error has been reached
  */
 function checkTooManyDigits () {
-    return tooManyDigits;
+    return this.tooManyDigits;
 }
 
 /**
@@ -407,5 +427,5 @@ function checkTooManyDigits () {
  * @returns {boolean} Whether a divide by zero error has been reached
  */
 function checkDivideByZero () {
-    return divideByZero;
+    return this.divideByZero;
 }
